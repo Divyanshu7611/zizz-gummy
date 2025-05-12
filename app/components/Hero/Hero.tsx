90c8b13a3a4bbf6bb3d858bcde05704830489a80
Divyanshu <dshapbss202@gmail.com>
2025-05-13 00:41:40 +0530

    Hero Page Completed Responsive ness pending

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Image } from '@shopify/hydrogen';
+import Button from '../mini/Button';
+import { PiHandbagFill } from "react-icons/pi";
+import { BsArrowUpRightCircle } from "react-icons/bs";
 
 function Hero() {
   return (
@@ -9,17 +12,13 @@ function Hero() {
       {/* Overlayed content */}
       <div className="absolute top-1/4 left-10 max-w-md text-white space-y-4">
         <h3 className="text-[#2D2D2D] text-6xl font-extrabold">Wellness Just Got Tastier</h3>
-        <p className="text-[#2D2D2D] text-xl font-medium">
+        <p className="text-[#2D2D2D] text-xl font-medium mt-16">
           Say goodbye to boring pills. ZIZZ Gummies are your fun, flavorful fix for real health goals –
           better sleep, stronger hair, stronger bones, and more.
         </p>
-        <div className="flex gap-4 mt-5">
-          <button className="bg-green-600 text-white px-6 py-2 rounded hover:bg-green-700">
-            Shop Now
-          </button>
-          <button className="bg-transparent border border-white text-white px-6 py-2 rounded hover:bg-white hover:text-green-700">
-            Learn More
-          </button>
+        <div className="flex gap-4 mt-16">
+            <Button text='Shop Now' bgColor='bg-[#1E1E1E]' textColor='text-white' icon={<PiHandbagFill/>}/>
+          <Button text='Learn More' bgColor='bg-transparent' textColor='text-[#2D2D2D]'icon={<BsArrowUpRightCircle/>}/>
         </div>
       </div>
     </div>
---
+++ app/components/mini/Button.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+interface ButtonProps {
+  text: string;
+  bgColor?: string;
+  textColor?: string;
+  hoverBgColor?: string;
+  hoverTextColor?: string;
+  icon?: React.ReactNode;
+  onClick?: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  text,
+  bgColor = 'bg-green-600',
+  textColor = 'text-white',
+  hoverBgColor = '',
+  hoverTextColor = '',
+  icon,
+  onClick,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`flex items-center gap-2 px-6 py-2 rounded transition transform duration-300 ease-in-out border-2 font-medium   
+        ${bgColor} ${textColor} ${hoverBgColor} ${hoverTextColor} hover:scale-105`}
+    >
+      <span>{text}</span>
+      {icon && <span className="text-xl">{icon}</span>}
+    </button>
+  );
+};
+
+export default Button;
