4eef79329d6997b0aa8fadb5418b0ddf2ebc6f0f
Divyanshu <dshapbss202@gmail.com>
2025-05-17 02:06:30 +0530

    landing page updation

--- app/components/mini/Button.tsx
+++ app/components/mini/Button.tsx
@@ -22,7 +22,7 @@ const Button: React.FC<ButtonProps> = ({
   return (
     <button
       onClick={onClick}
-      className={`flex items-center gap-2 px-6 py-2 rounded transition transform duration-300 ease-in-out border-2 font-medium   
+      className={`flex items-center gap-2 px-6 py-2 rounded transition transform duration-300 ease-in-out border-2 font-medium poppins   
         ${bgColor} ${textColor} ${hoverBgColor} ${hoverTextColor} hover:scale-105`}
     >
       <span>{text}</span>
